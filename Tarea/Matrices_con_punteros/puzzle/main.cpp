a4b8cd2811f57719732e0653dc1737b53c792fc7
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-10-21 09:55:38 -0500

    creando la matriz

--- Tarea/Matrices_con_punteros/puzzle/main.cpp
+++ Tarea/Matrices_con_punteros/puzzle/main.cpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
+void matriz_ordenada(int (*p)[4],int tam)
+{
+    int cont=1;
+    for(int i=0;i<tam;i++)
+    {
+        for(int j=0;j<tam;j++)
+        {
+            *(*(p+i)+j)=cont;
+            cont++;
+        }
+    }
+}
+
+void imprimir_matriz(int (*p)[4],int tam)
+{
+    for(int i=0;i<tam;i++)
+    {
+        for(int j=0;j<tam;j++)
+        {
+            cout<<" "<<*(*(p+i)+j);
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
+    int matriz[4][4];
+    matriz_ordenada(matriz,4);
+    imprimir_matriz(matriz,4);
     cout << "Hello world!" << endl;
     return 0;
 }
