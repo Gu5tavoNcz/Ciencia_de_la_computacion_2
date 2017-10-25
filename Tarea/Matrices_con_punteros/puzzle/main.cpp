7950dbcffe69be66d75f2717ddb6514e32e69298
Gustavo <gustavo_ncz93@outlook.com>
2017-10-25 17:18:54 -0500

    Ya desordena

--- Tarea/Matrices_con_punteros/puzzle/main.cpp
+++ Tarea/Matrices_con_punteros/puzzle/main.cpp
@@ -1,5 +1,14 @@
+//
+//  main.cpp
+//  Puzzle_mac
+//
+//  Created by Gustavo Ñaupa Canaza on 25/10/17.
+//  Copyright © 2017 Gustavo Ñaupa Canaza. All rights reserved.
+//
+
 #include <iostream>
 #include <stdlib.h>
+#include<time.h>
 
 using namespace std;
 
@@ -37,19 +46,69 @@ void imprimir_matriz(int (*p)[4],int tam)
     }
 }
 
-void desordenar(int (*p)[4],int tam)
+void desordenar(int (*p)[4],int movimientos)
 {
-    int direccion=rand()%4;
+    srand(time(NULL));
+    int fila=3;
+    int columna=3;
+    while(movimientos>0)
+    {
+        int direccion=rand()%4;
+        switch (direccion)
+        {
+            case 0:
+                if((columna-1)>=0)
+                    {
+                    cambio(*(p+fila)+columna,*(p+fila)+columna-1);
+                    columna--;
+                }
+                break;
+
+            case 1:
+                if((fila-1)>=0)
+                {
+                    cambio(*(p+fila)+columna,*(p+fila-1)+columna);
+                    fila--;
+                }
+                break;
+
+            case 2:
+                if((columna+1)<4)
+                {
+                    cambio(*(p+fila)+columna,*(p+fila)+columna+1);
+                    columna++;
+                }
+                break;
+
+            case 3:
+                if((fila+1)<4)
+                {
+                    cambio(*(p+fila)+columna,*(p+fila+1)+columna);
+                    fila++;
+                }
+                break;
+
+            default:
+                break;
+        }
+        movimientos--;
+        //cout<<"\n"<<direccion<<endl;
+    }
+
+
 }
 
 
 int main()
 {
+
     int matriz[4][4];
     matriz_ordenada(matriz,4);
+    desordenar(matriz,100);
     imprimir_matriz(matriz,4);
-
-    //cout << "Hello world!" << endl;
+    int hola=rand()%4;
+    cout <<"\n"<<hola<< endl;
     return 0;
 
 }
+
