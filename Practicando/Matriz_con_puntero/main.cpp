7754e3f0719e0557cb4e5878c4467ac88cae95fe
Gustavo <gustavo_ncz93@outlook.com>
2017-11-03 13:44:44 -0500

    actualizando

--- Practicando/Matriz_con_puntero/main.cpp
+++ Practicando/Matriz_con_puntero/main.cpp
@@ -2,8 +2,50 @@
 
 using namespace std;
 
+void llenarM(int (*p)[4])
+{
+    int (*f)[4]=p;
+    int *pc, *c;
+    pc=*p;
+    c=pc;
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            //cout<<"'"<<**f<<"'";
+            cout<<*pc<<",";
+            pc++;
+        }
+        f++;
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+   int M[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+   int (*A)[4] = M;
+   int (*a)[4] = A;
+   int *B, *b;
+   int i,j;
+   llenarM(M);
+/*
+   A++;
+   B=*A;
+   b=B;
+   B+=2;
+
+   cout<<*B<<endl;
+
+   i =  A-a;
+   j = B - b;
+
+    cout<<i<<","<<j<<endl;
+
+    B = b;
+    B += 5;
+
+    cout<<*B<<endl;
+
     return 0;
+    */
 }
